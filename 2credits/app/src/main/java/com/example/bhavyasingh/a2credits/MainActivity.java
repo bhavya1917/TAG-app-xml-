a29a6c184e30967c55b25e5ca35c2ef738372ba3
Bhavya Singh <bhavyarowdy@gmail.com>
2016-12-19 02:04:25 +0530

    2 credits
    2 credit activity.

---
+++ 2credits/app/src/main/java/com/example/bhavyasingh/a2credits/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.bhavyasingh.a2credits;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
